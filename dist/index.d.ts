c96f008485699734d4c4ebc2d7bfd49ddffaf421
Palsson <jaaacke@gmail.com>
2020-03-02 16:47:39 +0100

    test

---
+++ dist/index.d.ts
@@ -0,0 +1,29 @@
+export declare enum FileImportStatus {
+    Error = 0,
+    Completed = 1,
+    Sending = 2
+}
+export declare enum FileType {
+    Teleopti = 1
+}
+export interface FileImportSetting {
+    type: string;
+    name: string;
+    value: string;
+}
+export declare enum FileImportCellType {
+    Workday = 1,
+    Absence = 2
+}
+export declare enum FileImportCellStatus {
+    Error = 1,
+    Sending = 2,
+    Sent = 3,
+    Ready = 4
+}
+export declare enum CalendarItemType {
+    Workday = "Workday",
+    CreditedWorkday = "CreditedWorkday",
+    AbsencePeriod = "AbsencePeriod",
+    AbsenceDay = "AbsenceDay"
+}
---
+++ dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CalendarItemType = exports.FileImportCellStatus = exports.FileImportCellType = exports.FileType = exports.FileImportStatus = void 0;
+var FileImportStatus;
+(function (FileImportStatus) {
+    FileImportStatus[FileImportStatus["Error"] = 0] = "Error";
+    FileImportStatus[FileImportStatus["Completed"] = 1] = "Completed";
+    FileImportStatus[FileImportStatus["Sending"] = 2] = "Sending";
+})(FileImportStatus = exports.FileImportStatus || (exports.FileImportStatus = {}));
+var FileType;
+(function (FileType) {
+    FileType[FileType["Teleopti"] = 1] = "Teleopti";
+})(FileType = exports.FileType || (exports.FileType = {}));
+var FileImportCellType;
+(function (FileImportCellType) {
+    FileImportCellType[FileImportCellType["Workday"] = 1] = "Workday";
+    FileImportCellType[FileImportCellType["Absence"] = 2] = "Absence";
+})(FileImportCellType = exports.FileImportCellType || (exports.FileImportCellType = {}));
+var FileImportCellStatus;
+(function (FileImportCellStatus) {
+    FileImportCellStatus[FileImportCellStatus["Error"] = 1] = "Error";
+    FileImportCellStatus[FileImportCellStatus["Sending"] = 2] = "Sending";
+    FileImportCellStatus[FileImportCellStatus["Sent"] = 3] = "Sent";
+    FileImportCellStatus[FileImportCellStatus["Ready"] = 4] = "Ready";
+})(FileImportCellStatus = exports.FileImportCellStatus || (exports.FileImportCellStatus = {}));
+var CalendarItemType;
+(function (CalendarItemType) {
+    CalendarItemType["Workday"] = "Workday";
+    CalendarItemType["CreditedWorkday"] = "CreditedWorkday";
+    CalendarItemType["AbsencePeriod"] = "AbsencePeriod";
+    CalendarItemType["AbsenceDay"] = "AbsenceDay";
+})(CalendarItemType = exports.CalendarItemType || (exports.CalendarItemType = {}));
