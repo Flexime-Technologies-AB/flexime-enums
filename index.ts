ec32e55e96368a79c5d7af76f12e9e712a6cc394
Robin <robinsilverhav@gmail.com>
2020-03-16 14:05:43 +0100

    added FilImportCellError enum

--- index.ts
+++ index.ts
@@ -28,3 +28,24 @@ export enum CalendarItemType {
   AbsencePeriod = "AbsencePeriod",
   AbsenceDay = "AbsenceDay"
 }
+export enum FileImportCellError {
+  IpPersonDoesNotExist = 1,
+  IpArticleDoesNotExist = 2,
+  IpOrderDoesNotExist = 3,
+  IpAbsenceCodeDoesNotExist = 4,
+  IncorrectCellType = 5,
+  MissingArticleMapping = 6,
+  MissingPersonMapping = 7,
+  MissingOrderMapping = 8,
+  MissingAbsenceCodeMapping = 9,
+  MissingDateFrom = 10,
+  MissingDateTo = 11,
+  FailedToCreateIpWorkday = 12,
+  FailedToCreateIpAbsence = 13,
+  WorkdayWithErrorSameDay = 14,
+  FailedToCreateIpAbsencePeriod = 15,
+  MissingIpObjectId = 16,
+  FailedToAddIpArticles = 17,
+  FailedToAttestIpWorkday = 18
+}
+
